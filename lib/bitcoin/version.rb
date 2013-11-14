b4a48b887a52243ca0ff381f2f37835377f7573d
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2013-11-14 14:25:50 -0500

    bump patch

--- lib/bitcoin/version.rb
+++ lib/bitcoin/version.rb
@@ -2,7 +2,7 @@ module Bitcoin
   module Version
     MAJOR = 0
     MINOR = 0
-    PATCH = 2
+    PATCH = 3
     REL = nil
     
     STRING = REL ? [MAJOR, MINOR, PATCH, REL].join('.') : [MAJOR, MINOR, PATCH].join('.')
