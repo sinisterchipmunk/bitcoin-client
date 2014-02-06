48797865778bcae3f477fbbba0fd8a7aef930e5e
Ramon Martinez <rampa@encomix.org>
2014-02-06 14:53:51 +0100

    Changed gem name to bitcoin-client

--- bitcoin-client.gemspec
+++ bitcoin-client.gemspec
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 $:.push File.expand_path("../lib", __FILE__)
-require "bitcoin/version"
+require "bitcoin-client/version"
 
 Gem::Specification.new do |s|
   s.licenses    = [ 'MIT' ]
--- lib/bitcoin-client.rb
+++ lib/bitcoin-client.rb
@@ -1,1 +1,21 @@
-require File.expand_path("bitcoin", File.dirname(__FILE__))
+require File.expand_path("bitcoin-client", File.dirname(__FILE__))
+
+module Bitcoin
+  autoload :Client, 'bitcoin-client/client'
+  autoload :API,    'bitcoin-client/api'
+  autoload :Request,'bitcoin-client/request'
+  autoload :RPC,    'bitcoin-client/rpc'
+  autoload :Errors, 'bitcoin-client/errors'
+  autoload :Version,'bitcoin-client/version'
+  autoload :VERSION,'bitcoin-client/version'
+  autoload :DSL,    'bitcoin-client/dsl'
+  
+  def self.included(base)
+    base.send(:include, Bitcoin::DSL)
+    base.send(:extend,  Bitcoin::DSL)
+  end
+end
+
+def Bitcoin(user, pass, options = {})
+  ::Bitcoin::Client.new(user, pass, options)
+end
